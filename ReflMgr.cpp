0395079c74757bafb7e32889b8691eb676a69af4
heptagon196 <heptagon196@outlook.com>
2023-03-28 16:50:20 +0800

    update

--- ReflMgr.cpp
+++ ReflMgr.cpp
@@ -162,14 +162,14 @@ bool ReflMgr::HasClassInfo(TypeID type) {
 }
 
 static inline std::string_view removeNameRefAndConst(std::string_view name) {
-    int start = 0, end = name.length() - 1;
+    int start = 0, end = name.length();
     if (name.length() > 6 && name.substr(0, 6) == "const ") {
         start += 6;
     }
     if (name[name.length() - 1] == '&') {
         end--;
     }
-    return name.substr(start, end - start);
+    return name.substr(start, std::max(0, end - start));
 }
 
 SharedObject ReflMgr::New(TypeID type, const std::vector<ObjectPtr>& args) {
